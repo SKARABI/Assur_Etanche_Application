b22120d23cbdf15d8d3009505b0f51e948e82934
SKARABI <skarabifounder@gmail.com>
2018-02-07 20:12:53 +0000

    I add two routes in my file app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
-
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
+#MAIN PAGE OF THE APPLICATION
+@app.route("/")
 def index():
-    return'<h1>Hello SKARABI!</h1>'
+    return '<h1>SKARABI</h1>'
+
+#DYNAMIC ROOT
+@app.route('/user/<name>')
+def user(name):
+    return'<h1>Hello,%s!</h1>'%name
+
 
 if __name__ == '__main__':
     app.run(debug=True)
